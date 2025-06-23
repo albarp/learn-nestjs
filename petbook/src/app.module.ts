09a75b07fbd62ed491cca01071ab4eeffc1fc249
albarp <alberto.arpini@gmail.com>
2025-06-23 13:31:59 +0000

    logger middleware

--- petbook/src/app.module.ts
+++ petbook/src/app.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { PetsModule } from './pets/pets.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [PetsModule],
   controllers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('pets');
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'pets', method: RequestMethod.GET });
+  }
+}
